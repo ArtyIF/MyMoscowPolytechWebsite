101a1b19007586235f35f784c22901933f70280a
Artyom Fomin <artyomisflash@mail.ru>
2020-11-24 21:50:45 +0300

    упс

--- site/view.php
+++ site/view.php
@@ -18,9 +18,9 @@
             <?php
                 $disciplines = get_all_lab_works();
                 $labWorksShort = [];
-                foreach ($disciplines => $discipline) {
-                    foreach ($discipline->childLabWorks => $labWork) {
-                        foreach ($labWork->childSectionIDs => $sectionID) {
+                foreach ($disciplines as $discipline) {
+                    foreach ($discipline->childLabWorks as $labWork) {
+                        foreach ($labWork->childSectionIDs as $sectionID) {
                             array_push($labWorksShort, [$discipline->$disciplineID, $labWork->$labWorkID, $sectionID]);
                         }
                     }
