09f605909fed65af2c75aa620dc72da2175f3614
Artyom Fomin <artyomisflash@mail.ru>
2020-11-24 13:22:05 +0300

    Добавил возможность обновлять сайт без PuTTY

---
+++ site/updatesite.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Обновление сайта с GitHub</title>
+</head>
+<body>
+    <h1>Обновление сайта с GitHub</h1>
+    <h2>Вывод команды</h2>
+    <?php
+    echo '<pre><samp>';
+    echo exec('git pull');
+    echo '</samp></pre>';
+    ?>
+</body>
+</html>
