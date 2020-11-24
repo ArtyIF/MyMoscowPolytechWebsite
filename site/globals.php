67e34f85113380158577503c6dee8b06d68f1e7f
Artyom Fomin <artyomisflash@mail.ru>
2020-11-24 17:41:55 +0300

    Оно неправильно работает, когда задания не в порядке

--- site/globals.php
+++ site/globals.php
@@ -1,5 +1,5 @@
 <?php
-$disciplineList = [
+$discdict = [
     "poya" => "Проблемно-ориентированные языки",
     "vss" => "Вычислительные системы и сети",
     "webapp" => "Разработка веб-приложений",
@@ -7,20 +7,13 @@ $disciplineList = [
     // "название папки дисциплины" => "человеко-читабельное название дисциплины",
 ];
 
-function get_disc_human_name(string $disciplineID) {
-    global $disciplineList;
-    if (array_key_exists($disciplineID, $disciplineList)) {
-        return $disciplineList[$disciplineID];
+function get_disc_human_name(string $discid)
+{
+    global $discdict;
+    if (array_key_exists($discid, $discdict)) {
+        return $discdict[$discid];
     } else {
-        return "ОШИБКА: неизвестный ID дисциплины: " . $disciplineID;
+        return "ОШИБКА: неизвестный ID дисциплины: " . $discid;
     }
-}
-
-function get_lab_works_count(string $disciplineID) : int {
-    return count(glob("iframes/" . $disciplineID . "/lr*", GLOB_ONLYDIR));
-}
-
-function get_sections_count(string $disciplineID, int $labWorkID) : int {
-    return count(glob("iframes/" . $disciplineID . "/lr" . $labWorkID . "/*.html"));
 }
 ?>
